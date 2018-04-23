bfea65d4df6c11d2265b537ae84863fdb370c247
Mikaal Naik <matthewnaik@gmail.com>
2018-04-23 22:19:09 +0000

    character counting remaining characters

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+      console.log( "document loaded" )
+
+   });
+
+   $( window ).on( "load", function() {
+       console.log( "window loaded" );
+   });
+
+   $( document ).ready(function() {
+    var test = $('.counter').text()
+       console.log(test);
+   });
+
+
+// display remaining characters on Counter and turn red if over 140
+   $(document).ready(function(){
+        $(".new-tweet textarea").keyup(function(){
+           var input = $(this).val();
+           var remainingChars = 140 - input.length
+           if(remainingChars < 0){
+             $(this).parents().find('.counter').css('color', 'red')
+           }
+           $(".counter").text(remainingChars);
+       });
+   });
